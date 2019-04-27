cd42cadbab62354b5811b925c3e866666c20ff18
ajpowell021 <ajpowell021@gmail.com>
2019-04-26 22:38:29 -0400

    Add class manager and age manager.

---
+++ LD44/Assets/Code/AgeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgeManager : MonoBehaviour {
+
+    public float ageRate;
+    public float currentAge;
+    public GameObject ageTextObject;
+
+    private TextMeshProUGUI ageText;
+
+    private void Awake() {
+        ageText = ageTextObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update() {
+        currentAge += Time.deltaTime * ageRate;
+        setAgeText();
+    }
+
+    private void setAgeText() {
+        ageText.text = "Age: " + Mathf.FloorToInt(currentAge) + " years";
+    }
+}
---
+++ LD44/Assets/Code/ClassManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassManager : MonoBehaviour {
+
+    public static ClassManager instance;
+    
+    // Classes
+    public AgeManager ageManager;
+    
+    private void Awake() {
+        setInstance();
+        setClasses();
+    }
+
+    private void setInstance() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    private void setClasses() {
+        ageManager = gameObject.GetComponent<AgeManager>();
+    }
+}
